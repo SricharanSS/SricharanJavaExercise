2bf8ca059fdb0ad176b9b5dc0a2f4822700e9351
SricharanSS <125631907+SricharanSS@users.noreply.github.com>
2023-03-04 23:39:31 +0530

    Add Exercise 8 & 9 folders

---
+++ Exercise8/Main.java
@@ -0,0 +1,7 @@
+package Exercise8;
+
+class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Exercise9/Main.java
@@ -0,0 +1,10 @@
+package Exercise9;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+
+        String stringArray[] = 
+    }
+}
