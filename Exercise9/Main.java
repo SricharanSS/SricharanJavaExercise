a04ea17d4e61bc5235d87e72abe1f5e72b814a96
SricharanSS <125631907+SricharanSS@users.noreply.github.com>
2023-03-05 18:55:04 +0530

    Add Exercise9

--- Exercise9/Main.java
+++ Exercise9/Main.java
@@ -4,7 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         String str = scan.nextLine();
-
-        String stringArray[] = 
+        String stringArray[] = str.split(" ");
+        
+        int count = 0;
+        stringArray[0].trim();
+        for(int i=0; i<stringArray.length; i++) {
+            if( !(stringArray[i].equals("")) ) {
+               count++;
+            }
+        }
+        System.out.println("Words : "+count);
     }
 }
