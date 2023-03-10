00e7a143b7e8d91e588014f72cbf4cfe43331b56
SricharanSS <125631907+SricharanSS@users.noreply.github.com>
2023-03-10 14:31:40 +0530

    Add User Inputs for speed and distance

--- TruckProblem/Main.java
+++ TruckProblem/Main.java
@@ -1,10 +1,18 @@
 package TruckProblem;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Truck truck = new Truck();
-        truck.setSpeed(35.5);
-        truck.setDistance(600);
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Enter Speed : ");
+        int speed = scan.nextInt();
+        
+        System.out.print("Enter Distance : ");
+        int distance = scan.nextInt();
+        
+        truck.setSpeed(speed);
+        truck.setDistance(distance);
 
         TravelEstimator estimator = new TravelEstimator(truck);
         estimator.estimateArrival();
